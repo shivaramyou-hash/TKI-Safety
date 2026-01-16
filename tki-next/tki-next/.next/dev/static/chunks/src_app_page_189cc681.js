b42e4a65c7f09ac98e6883e1291b448661986b9d
Mukesh Kokkula <100822526+Mukeshkokkula5@users.noreply.github.com>
2026-01-17 01:03:34 +0530

    removed

--- tki-next/tki-next/.next/dev/static/chunks/src_app_page_189cc681.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_d30f4a23._.js",
-  "static/chunks/node_modules_83d76a5d._.js"
-],
-    source: "dynamic"
-});
